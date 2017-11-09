c09de4f8736ab4515f8bba33a904e5bb8f6cbb44
Wojciech <wojciechadamski71@gmail.com>
2017-11-10 00:17:35 +0100

    wersja2

---
+++ wersja2/quiz tablica/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace quiz_tablica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(501, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(501, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(12, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Prawda";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(285, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Falsz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(442, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(71, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(353, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Punktacja:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 259);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ wersja2/quiz tablica/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace quiz_tablica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int pkt;
+        string[] tablica_pytania = new string[4];
+        Random r = new Random();
+
+
+
+
+        private void button1_Click(object sender, EventArgs e) // Start
+        {
+            pkt = 0;
+            textBox2.Text = pkt.ToString();
+
+            tablica_pytania[0] = File.ReadLines(@"test.txt").Skip(1).Take(1).First();
+            tablica_pytania[1] = File.ReadLines(@"test.txt").Skip(2).Take(1).First();
+            tablica_pytania[2] = File.ReadLines(@"test.txt").Skip(3).Take(1).First();
+            tablica_pytania[3] = File.ReadLines(@"test.txt").Skip(4).Take(1).First();
+
+            textBox1.Text = tablica_pytania[r.Next(0, 4)];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // tak
+        {
+            if (textBox1.Text == tablica_pytania[0])
+            {
+                pkt = pkt + 1;
+                textBox1.Text = tablica_pytania[r.Next(0, 4)];
+                textBox2.Text = pkt.ToString();
+            }
+            if (textBox1.Text == tablica_pytania[1])
+            {
+                pkt = pkt + 1;
+                textBox1.Text = tablica_pytania[r.Next(0, 4)];
+                textBox2.Text = pkt.ToString();
+            }
+            else
+            {
+                pkt = pkt - 1;
+                textBox2.Text = pkt.ToString();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) // nie
+        {
+            if (textBox1.Text == tablica_pytania[2])
+            {
+                pkt = pkt + 1;
+                textBox1.Text = tablica_pytania[r.Next(0, 4)];
+                textBox2.Text = pkt.ToString();
+            }
+            if (textBox1.Text == tablica_pytania[3])
+            {
+                pkt = pkt + 1;
+                textBox1.Text = tablica_pytania[r.Next(0, 4)];
+                textBox2.Text = pkt.ToString();
+            }
+            else
+            {
+                pkt = pkt - 1;
+                textBox2.Text = pkt.ToString();
+            }
+
+        }
+    }
+}
